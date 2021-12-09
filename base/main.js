9d0f63aade57f1ffe24fc7271c31b65f885970af
StefanK <57302957+StefanKarasmilov@users.noreply.github.com>
2021-12-09 20:17:36 +0100

    NameSpaces done

--- base/main.js
+++ base/main.js
@@ -1,45 +1,11 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['Súper velocidad', 'Viajar en el tiempo']
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return text.length > 3;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    Validations.validateDate = (fecha) => {
+        return isNaN(fecha.valueOf());
     };
-})();
-(() => {
-    class Mutant {
-        constructor(name, realName) {
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return '';
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Stefan',
-        age: 27,
-        address: {
-            id: 125,
-            zip: 'KY2 SUD',
-            city: 'Ottawa'
-        },
-        getFullAddress: (id) => {
-            return id;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
 //# sourceMappingURL=main.js.map
---
+++ base/namespaces/validations.ts
@@ -0,0 +1,10 @@
+namespace Validations {
+  export const validateText = (text: string) => {
+    return text.length > 3
+  }
+
+  export const validateDate = (fecha: Date) => {
+    return isNaN(fecha.valueOf())
+  }
+}
+
